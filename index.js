2467674b306739c9e13946f12c2c4c941e416284
Jason <jason.zhu25@compscihigh.org>
2024-11-14 15:47:34 +0000

    almost done, need to force that chatGPT call run first before the rest of the code

--- index.js
+++ index.js
@@ -3,28 +3,18 @@ const prompt = require('prompt-sync')()
 
 const chatGPT = "https://api.openai.com/v1/chat/completions"
 const chatGPTKey = process.env.CHATGPT_API_KEY
-const tasty = "https://tasty.p.rapidapi.com/recipes/list?from=0&size=5&q="
-
-let condition = "yes"
+let randomDish = "https://foodish-api.com/api/images/"
+let dishLength = randomDish.length
 
 console.log("Heyyy this is a mealBot. If you don't what to eat, ask me with the type of food you want. ")
 console.log("You can choose between breakfast, lunch, dinner. ")
 console.log("Type your request in full sentence. ")
 let userMeal = prompt("")
-let forTasty; 
-
-while (condition != "no") {
-    if (userMeal.includes("breakfast")){
-        forTasty = "breakfast" 
-    } else if (userMeal.includes("lunch")) {
-        forTasty = "lunch"
-    } else if (userMeal.includes("dinner")) {
-        forTasty = "dinner"
-    }
-}
 
 let userAsk = {
-    messages: [{ role: "system", content: userMeal }], 
+    messages: [{ role: "system", 
+        content: userMeal + ", one item with the recipe only"
+    }], 
     model: "gpt-4o", 
 }
 
@@ -49,4 +39,64 @@ async function askChatGPT(userAsk){
         console.log("An error occurred: " + error.message)
     }
 }
+
 askChatGPT(userAsk)
+
+console.log("You can also check how the meal looks like by typing the name. You can choose only between biryani, burger, butter-chicken (pls enter the - sign as well or you're gonna stuck here forever), dessert, dosa, idly, pasta, pizza, rice, samosa. This random dish image API is limited so yk...")
+let dishName = prompt("")
+const dishList = ["biryani", "burger", "butter-chicken", "dessert", "dosa", "idly", "pasta", "pizza", "rice", "samosa"]
+dishName = dishName.toLocaleLowerCase()
+while (dishList.includes(dishName) == false) {
+    console.log("Invalid dish name, pls enter the correct one. ")
+    dishName = prompt("").toLocaleLowerCase()
+}
+randomDish = randomDish + dishName
+
+async function getDishImage(dishURL) {
+    try {
+        const response = await fetch(dishURL)
+        if (response.ok === false) {
+            console.log("HTTP error! Status: " + response.status)
+        }
+        const data = await response.json()
+        console.log(data["image"])
+    }
+    catch (error) {
+        console.log("An error occurred: " + error.message)
+    }
+}
+getDishImage(randomDish)
+let conditionForImage; 
+
+console.log("Do you want another meal? Type yes to continue, no to exit: ")
+let conditionForMeal = prompt("")
+
+while (true) {
+    if (conditionForMeal == "yes") {
+        console.log("Yesssssss, what do you want this time: ")
+        userMeal = prompt("")
+        askChatGPT(userAsk)
+
+        console.log("Another image too? ")
+        conditionForImage = prompt("")
+        if (conditionForImage == "yes") {
+            console.log("Okay type the dish name: ")
+            let modifiedDishName = prompt()
+            modifiedDishName = modifiedDishName.toLocaleLowerCase()
+            dishName = dishName.slice(0, dishLength)
+            dishName = dishName + modifiedDishName 
+            getDishImage(dishName)
+        }
+
+        console.log("Do you want another meal? Type yes to continue, no to exit: ")
+        conditionForMeal = prompt("")
+    } else if (conditionForMeal == "no") {
+        console.log("Okay bye or whatever. ")
+        return; 
+    } else {
+        console.log("Invalid response, please enter yes or no to continue: ")
+        conditionForMeal = prompt("")
+    }
+}
+
+console.log("Okay so this is the end of this mealBot, Bon Appetit I guess")
