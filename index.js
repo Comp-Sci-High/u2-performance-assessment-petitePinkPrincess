f471b7338c009bc9f2f751ce3acc9efcbc541706
Jason <jason.zhu25@compscihigh.org>
2024-11-15 19:33:14 +0000

    some modifications

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const chatGPTKey = process.env.CHATGPT_API_KEY
 let randomDish = "https://foodish-api.com/api/images/"
 let dishLength = randomDish.length
 
+console.log("Heyyy this is a mealBot. If you don't what to eat, ask me with the type of food you want. ")
+console.log("You can choose between breakfast, lunch, dinner. ")
+console.log("Type your request in full sentence. ")
 
 // fetch and POST on chatGPT
 async function askChatGPT(userAsk) {
@@ -33,11 +36,23 @@ async function askChatGPT(userAsk) {
         console.log("An error occurred: " + error.message)
     }
 }
+
+// getting Foodish info
+async function getDishImage(dishURL) {
+    try {
+        const response = await fetch(dishURL)
+        if (response.ok === false) {
+            console.log("HTTP error! Status: " + response.status)
+        }
+        const data = await response.json()
+        console.log(data["image"])
+    } catch (error) {
+        console.log("An error occurred: " + error.message)
+    }
+}
+
 async function main() {
     // getting user info
-    console.log("Heyyy this is a mealBot. If you don't what to eat, ask me with the type of food you want. ")
-    console.log("You can choose between breakfast, lunch, dinner. ")
-    console.log("Type your request in full sentence. ")
     let userMeal = prompt("")
 
     // request body for chatGPT
@@ -54,11 +69,11 @@ async function main() {
     console.log("You can also check how the meal looks like by typing the name. You can choose only between biryani, burger, butter-chicken (pls enter the - sign as well or you're gonna stuck here forever), dessert, dosa, idly, pasta, pizza, rice, samosa. This random dish image API is limited so yk...")
     let dishName = prompt("")
     const dishList = ["biryani", "burger", "butter-chicken", "dessert", "dosa", "idly", "pasta", "pizza", "rice", "samosa"]
-    dishName = dishName.toLocaleLowerCase()
+    dishName = dishName.toLowerCase()
     // checking invalid info 
     while (dishList.includes(dishName) == false) {
         console.log("Yeah I know you wanna get something special, but we don't have it, so pls enter the correct one. ")
-        dishName = prompt("").toLocaleLowerCase()
+        dishName = prompt("").toLowerCase()
     }
     randomDish = randomDish + dishName
     getDishImage(randomDish)
@@ -80,7 +95,7 @@ async function main() {
             if (conditionForImage == "yes") {
                 console.log("Okay type the dish name: ")
                 let modifiedDishName = prompt()
-                modifiedDishName = modifiedDishName.toLocaleLowerCase()
+                modifiedDishName = modifiedDishName.toLowerCase()
                 dishName = dishName.slice(0, dishLength)
                 dishName = dishName + modifiedDishName
                 getDishImage(dishName)
@@ -100,18 +115,3 @@ async function main() {
     console.log("So this is the end of this mealBot, Bon Appetit I guess... ")
 }
 main()
-
-
-// getting Foodish info
-async function getDishImage(dishURL) {
-    try {
-        const response = await fetch(dishURL)
-        if (response.ok === false) {
-            console.log("HTTP error! Status: " + response.status)
-        }
-        const data = await response.json()
-        console.log(data["image"])
-    } catch (error) {
-        console.log("An error occurred: " + error.message)
-    }
-}
